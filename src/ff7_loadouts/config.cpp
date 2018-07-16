3e24526a9a4e7be706fb8e7e70ff28de358f082e
Marcel Lof <marcelrlof@gmail.com>
2018-07-16 21:05:17 +0200

    Added FF7 Loadouts

---
+++ src/ff7_loadouts/config.cpp
@@ -0,0 +1,25 @@
+class CfgPatches
+{
+	class ff7_loadouts
+	{
+		name="FoxForce7 Loadouts";
+		version=1.2;
+		units[]={};
+		weapons[]={};
+		requiredVersion=1.8;
+		requiredAddons[]=
+		{
+			"ff7_gear_vsm",
+		};
+		author="FoxForce7";
+		authors[]=
+		{
+			"mlof"
+		};
+		magazines[]={};
+		ammo[]={};
+	};
+};
+
+#include "CfgRoles.sqf" 
+#include "CfgRespawnInventory.sqf"
