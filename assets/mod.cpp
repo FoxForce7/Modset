393a8c55555a81f5d0b61ce8bb6f99b214d0db9a
Marcel Lof <marcelrlof@gmail.com>
2018-07-17 07:56:44 +0200

    Added Assets directory

---
+++ assets/mod.cpp
@@ -0,0 +1,10 @@
+name = "GentleFox Base v05";
+picture = "mod_logo_512.paa";
+logo = "logo_bfox.paa";
+logoOver = "logo_ofox.paa";
+tooltip = "GentleFoxBase05";
+tooltipOwned = "GentleFox-Base";
+overview = "<t font='PuristaMedium'><t size='1.2' color='#ff7420' align='center'>GentleFox Base v03</t><br/><br/><t size='0.8'>this modpack contains:</t><br/><br/><t size='0.6' color='#aaeeff'>mods :D</t></t>";
+author = "FoxForce7";
+overviewPicture = "mod_logo_512.paa";
+version = "05";
